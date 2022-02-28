8e5beb4cc1ba5b33c4151e9bbbf0d3d531d40336
kellermack <kellermack1@gmail.com>
2022-02-28 12:30:40 -0500

    working on the images

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="resource.css">
+    <link rel="stylesheet" href="{{ url('css/resources.css') }}">
 </head>
 <body>
 
@@ -15,9 +15,9 @@
 <div>
     <h1>Click a picture to learn more!</h1>
     <section class="container">
-        <a href="{{url("varroa.blade.php")}}><img src="{{asset('css/images/varroadestructormite.jpeg class')}}="image2"></a>
-        <a href="hiveBeetle.php"><img src="Images/hiveBeetle.jpeg" class="image2"></a>
-        <a href="foulBrood.php"><img src="Images/foulbrood.jpeg" class="image2"></a>
+        <a href="{{url("varroa.blade.php")}}"><img src="{{asset('css/images/varroadestructormite.jpeg')}}" class="image2" alt="varroa"></a>
+        <a href="hiveBeetle.php"><img src="{{('/css/images/hiveBeetle.jpeg')}}" class="image2" alt="hivebeetle"></a>
+        <a href="foulBrood.php"><img src= "{{('/css/images/foulbrood.jpeg')}}" class="image2"></a>
         <a href="statePrograms.html"><img src="Images/unitedStates.jpeg" class="image2"></a>
         <a href="waxMoth.php"><img src="Images/waxMoth.jpeg" class="image2"></a>
         <a href="bearsandskunks.php"><img src="Images/skunkBear.jpeg" class="image2"></a>
