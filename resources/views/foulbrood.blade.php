e035c9987b149cfdc4394ee4e9ee3456615839e8
kellermack <kellermack1@gmail.com>
2022-03-14 10:51:12 -0400

    adding css

--- resources/views/foulbrood.blade.php
+++ resources/views/foulbrood.blade.php
@@ -15,16 +15,17 @@ session_start();
     <link rel="stylesheet" href="{{ url('css/resources.css') }}">
 </head>
 <body>
+<div class="foulbrood">
 <a href="{{url("welcome.blade.php")}}"><img src="{{('css/images/honeycomb.png')}}" alt="Bee hive" class="image1"></a>
 <h1>Something Smells Foul</h1>
 <p>FoulBrood, 4 different diseases that directly attack brood chambers of bee hives.
-    Consisting of American Foulbrood, European Foulbrood, Chalkbrood and Stonebrood.
-    The following table can help differentiate between the American and European Foulbrood.
+    Consisting of American Foul brood, European Foul brood, Chalkboard and Stone-brood.
+    The following table can help differentiate between the American and European Foul brood.
 </p>
 <h3>Symptoms</h3>
 <table>
     <tr>
-        <th>Apperance of Brood</th>
+        <th>Appearance of Brood</th>
         <th>Color Of Brood</th>
         <th>Consistency of Brood</th>
         <th>Odor</th>
@@ -47,9 +48,9 @@ session_start();
 
 </table>
 <h3>American Foul Brood</h3>
-<img src="Images/americanFoulbrood.jpeg" alt="foul-brood">
+<img src="{{('css/images/americanFoulbrood.jpeg')}}" alt="foul-brood">
 <h3>European FoulBrood</h3>
-<img src="Images/EuropeanFoulBrood.jpeg" alt="Euro Foul brood">
+<img src="{{('css/images/EuropeanFoulBrood.jpeg')}}" alt="Euro Foul brood">
 <h2>Treatments</h2>
 <p>A majority of states by law require you to contact the state beekeepers' association
     to report foul-brood. With the spores able to be viable for nearly 80 years the only real solution
@@ -62,7 +63,7 @@ session_start();
     and treating Chalkboard.
 </p>
 <h3>ChalkBrood Larvae</h3>
-<img src="Images/Chalkbrood.jpeg" alt="chalkboard">
+<img src="{{('css/images/Chalkbrood.jpeg')}}"alt="chalkboard">
 <h2>Stone-brood</h2>
 <p>Not as common as other fungal diseases, can be somewhat difficult to identify in early stages.
     Can form a collar like ring around the head of the larvae, after death they turn black and
@@ -70,7 +71,8 @@ session_start();
     of the larvae that are infected by the fungal spores.
 </p>
 <h3>Stone-brood Cells</h3>
-<img src="Images/stonebrood.jpeg" alt="stone brood">
+<img src="{{('css/images/stonebrood.jpeg')}}" alt="stone brood">
+</div>
 </body>
 
 {{--<?php
